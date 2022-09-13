ef9a551afdbfaac49337e5b20691568e6a42d22d
Muazzam Khan <muazzamkhan1802@icloud.com>
2022-09-13 20:12:28 +0500

    Many things to be done, 1. When add new note is pressed text area and input should refresh. 2. Clicking on buttons in the list should open that note. 3. Delete note should remove the note and its name from the list. 4. Add new note shouldnt allow empty title to get saved

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function saveNote(){
     let title = document.querySelector("#title").value;
     let content = document.querySelector("#Note").value;
     let arr;
-    if(localStorage.getItem("items")){
+    if(localStorage.getItem("items") != null){
         arr = JSON.parse(localStorage.getItem("items"));
         arr.push([title,content]);
         localStorage.setItem("items",JSON.stringify(arr));
@@ -12,6 +12,21 @@ function saveNote(){
         arr.push([title,content]);
         localStorage.setItem("items",JSON.stringify(arr));
     }
+
+    let str = "";
+    str = document.querySelector("table").innerHTML;
+    str += `
+                <tr>
+                <button class="listElement">${title}</button>
+                </tr>`;
+    document.querySelector("table").innerHTML = str;
+    
 }
 
+function clicked(){
+    console.log(`clicked on ${document.querySelector}`);
+}
 
+function addNote(){
+    
+}
