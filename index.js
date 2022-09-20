cd6eb439d1b7cb620534e4a7b2ae708bbf929aa2
Muazzam Khan <muazzamkhan1802@icloud.com>
2022-09-20 23:19:32 +0500

    Code refactoring

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 function displayList(){
-    let obj;
-    let str = "";
-    let titleArr;
+    let obj, titleArr, str = "";
+
     if(localStorage.getItem("items") != null){
         obj = JSON.parse(localStorage.getItem("items"));
         titleArr = Object.keys(obj);
+
         titleArr.forEach((element) => {
             str += `
                 <tr>
                 <button class="listElement" id="${element}" onclick=clicked(this.id)>${element}</button>
                 </tr>`;
         });
+
         document.querySelector("table").innerHTML = str;
     }
 }
 
 function displayNote(){
     let obj, titleArr, title, content, titleArea, textArea;
+
     obj = JSON.parse(localStorage.getItem("items"));
     titleArr = Object.keys(obj);
     title = titleArr[0];
@@ -28,11 +30,14 @@ function displayNote(){
 }
 
 function saveNote(){
-    let title = document.querySelector("#title").value;
-    let content = document.querySelector("#Note").value;
-    let obj;
+    let title, content, obj;
+
+    title = document.querySelector("#title").value;
+    content = document.querySelector("#Note").value;
+
     if(localStorage.getItem("items") != null){
         obj = JSON.parse(localStorage.getItem("items"));
+
         if(`${title}` in obj){
 
         }
@@ -46,13 +51,17 @@ function saveNote(){
         obj[`${title}`] = content;
         localStorage.setItem("items",JSON.stringify(obj));
     }
+
     displayList();
        
 }
 
 function addNote(){
-    let titleArea = document.querySelector("#title");
-    let textArea = document.querySelector("#Note");
+    let titleArea, textArea;
+
+    titleArea = document.querySelector("#title");
+    textArea = document.querySelector("#Note");
+
     if(document.querySelector("#title").value in JSON.parse(localStorage.getItem("items")) == false){
         let confirmed = confirm("This note is not yet saved, are you sure you want to make a new one, without saving this first?");
         if(confirmed){
@@ -67,34 +76,46 @@ function addNote(){
 }
 
 function deleteNote(){
-    let title = document.querySelector("#title").value;
-    let obj = JSON.parse(localStorage.getItem("items"));
+    let title, obj;
+
+    title = document.querySelector("#title").value;
+    obj = JSON.parse(localStorage.getItem("items"));
+
     delete obj[`${title}`];
+
     localStorage.setItem("items",JSON.stringify(obj));
     location.reload();
+
     displayList();
 }
 
 function clicked(id){
-    let obj = JSON.parse(localStorage.getItem("items"));
-    let title = id;
-    let content = obj[`${id}`];
+    let obj, title, content;
+
+    obj = JSON.parse(localStorage.getItem("items"));
+    title = id;
+    content = obj[`${id}`];
+
     if(document.querySelector("#title").value in JSON.parse(localStorage.getItem("items")) == false){
         let confirmed = confirm("This note is not yet saved, are you sure you want to continue without saving this first?");
+
         if(confirmed){
-            let titleArea = document.querySelector("#title");
-            let textArea = document.querySelector("#Note");
+            let titleArea, textArea;
+            titleArea = document.querySelector("#title");
+            textArea = document.querySelector("#Note");
             titleArea.value = title;
             textArea.value = content; 
         }
     }
     else{
-        let titleArea = document.querySelector("#title");
-        let textArea = document.querySelector("#Note");
+        let titleArea, textArea;
+        titleArea = document.querySelector("#title");
+        textArea = document.querySelector("#Note");
         titleArea.value = title;
         textArea.value = content;
     }
 }
+
 
 displayList();
 displayNote();
